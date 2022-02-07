35c1980447292a820e435ebd70ef4ad3488e9241
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2022-02-07 15:14:40 -0400

    evilCows mostly done

--- src/components/quiz/EvilCows.tsx
+++ src/components/quiz/EvilCows.tsx
@@ -1,8 +1,40 @@
+import { useContext, useEffect, useState } from 'react';
+import { QuizContext } from '../../context/AppContext';
+
 interface propsTypes {
 	setQuizBoxState: React.Dispatch<React.SetStateAction<boolean>>;
 	setEvilCowsState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function EvilCows(props: propsTypes) {
-	return <></>;
+	// QuizContext
+	const { quiz } = useContext(QuizContext);
+
+	// Background State
+	const [backgroundImage, setBackgroundImage] = useState(
+		"url('images/evil-cow.png')"
+	);
+
+	// Render Background image on background state change
+	useEffect(() => {
+		document.body.style.backgroundImage = backgroundImage;
+	}, [backgroundImage]);
+
+	async function finishedGame() {
+		await quiz.submitChoice('Continue?');
+		props.setQuizBoxState(true);
+		props.setEvilCowsState(false);
+	}
+
+	return (
+		<>
+			<header className="text-center">
+				<h1 style={{ color: 'red' }}>Oh no! There are evil cows everywhere?</h1>
+			</header>
+
+			<div className="text-center">
+				<button onClick={() => finishedGame()}>Continue?</button>
+			</div>
+		</>
+	);
 }
