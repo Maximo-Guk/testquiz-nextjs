13e6018190f31e6dcebc0db087c178c199e5871c
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2022-02-07 14:39:41 -0400

    Formatting

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -9,13 +9,11 @@ interface QuizProviderTypes {
 	children: React.ReactNode;
 }
 
-export const QuizContext = createContext<QuizContextTypes>(
-	{} as QuizContextTypes
-);
+export const QuizContext = createContext({} as QuizContextTypes);
 
 // Quiz provider wrapper contains logic for user class and quiz class
 export default function AuthProvider({ children }: QuizProviderTypes) {
-	const [quiz, setQuiz] = useState<Quiz>({} as Quiz);
+	const [quiz, setQuiz] = useState({} as Quiz);
 
 	return (
 		<QuizContext.Provider
